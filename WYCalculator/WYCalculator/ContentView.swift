2f09a3ca853335b466faec751d18cad9b1fe1811
黎炜烨 <liweiye.1997@bytedance.com>
2019-11-03 15:43:00 +0800

    First commit
    Change-Id: I3eb311b3eace4e918aad3a9ec72f116dbcc65577

---
+++ WYCalculator/WYCalculator/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  WYCalculator
+//
+//  Created by liweiye on 2019/11/3.
+//  Copyright © 2019 liweiye. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("+")
+            .font(.title)
+            .foregroundColor(.white)
+            .padding()
+            .background(Color.orange)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
